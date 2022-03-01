b222f10fb6f6003d2eb4c4c860fa1aa901021597
Zaw-Myo-Htun-1996 <zawmyohtun@gicjp.biz>
2022-03-01 11:58:53 +0630

    feat:friend crud complete

---
+++ app/models/friend.rb
@@ -0,0 +1,4 @@
+class Friend < ApplicationRecord
+    validates :name, :email, :phone , :twitter, presence: true
+    validates_uniqueness_of :email, on: [:create , :update], message: "must be unique"
+end
